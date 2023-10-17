f3413a73e93ee4bb38ae80290e7ebe48e1c4c617
Ruslan Abdulin <ruslan.abdulin@exness.com>
2023-10-17 22:58:18 +0300

    test_2

--- slack_test.py
+++ slack_test.py
@@ -1,36 +1,52 @@
 from flask import Flask, request, jsonify
-import requests  # Импортируем библиотеку requests
+import asyncio
 
 app = Flask(__name__)
 
-def test():
-    print("Функция test была вызвана!")
+async def test_1():
+    print("Функция test_1 была вызвана!")
+    return "Результат test_1"
+
+async def test_2():
+    print("Функция test_2 была вызвана!")
+    return "Результат test_2"
+
+async def test_3():
+    print("Функция test_3 была вызвана!")
+    return "Результат test_3"
 
 @app.route("/slack", methods=["POST"])
 def slack_endpoint():
     text = request.form.get('text', '')
     user_id = request.form.get('user_id', '')
-    test()
 
-    # # Отправка POST-запроса
-    # post_url = "https://test.com/test"
-    # payload = {"key": "value"}  # Пример тела запроса. Замените на нужное.
-    # headers = {"Content-Type": "application/json"}  # Заголовок запроса. Используется при отправке JSON данных.
-    # response = requests.post(post_url, json=payload, headers=headers)  # Отправка POST-запроса с JSON-данными
-    #
-    # # Можно обработать ответ от POST-запроса, если это необходимо
-    # if response.status_code == 200:
-    #     post_response_text = "POST-запрос успешно отправлен!"
-    # else:
-    #     post_response_text = f"Ошибка при отправке POST-запроса: {response.status_code}"
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    response_text = f"Привет <@{user_id}>! Вы сказали: {text}\n"
+
+    try:
+        if text == '1':
+            result = asyncio.run(test_1())
+            response_text += result
+        elif text == '2':
+            result = asyncio.run(test_2())
+            response_text += result
+        elif text == '3':
+            result = asyncio.run(test_3())
+            response_text += result
+        elif text.upper() == 'ALL':
+            # Запуск всех функций асинхронно и сбор результатов
+            tasks = [test_1(), test_2(), test_3()]
+            results = loop.run_until_complete(asyncio.gather(*tasks))
+            response_text += ', '.join(results)
+    finally:
+        loop.close()
 
     # Ответ в Slack
     return jsonify({
         "response_type": "in_channel",
-        "text": (
-            f"Привет <@{user_id}>! Вы сказали: {text}\n"
-            f"Зафиксировали!"
-        ),
+        "text": response_text
     })
 
 if __name__ == "__main__":
